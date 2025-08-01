addb20f9d70e5bafaf55b3928e0341cf307f5948
glk1001 <gregg.kay@gmail.com>
2025-08-01 17:30:42 +1000

    Trying out ruff on 'src/copy-reader-pngs-to-jpgs.py'.

--- src/copy-reader-pngs-to-jpgs.py
+++ src/copy-reader-pngs-to-jpgs.py
@@ -1,11 +1,10 @@
 import logging
-import os
 import shutil
 from pathlib import Path
 from typing import Callable
 
-from barks_fantagraphics.comics_consts import PNG_FILE_EXT, JPG_FILE_EXT
-from barks_fantagraphics.pil_image_utils import open_pil_image_for_reading, SAVE_JPG_COMPRESS_LEVEL
+from barks_fantagraphics.comics_consts import JPG_FILE_EXT, PNG_FILE_EXT
+from barks_fantagraphics.pil_image_utils import SAVE_JPG_COMPRESS_LEVEL, open_pil_image_for_reading
 
 # Setup basic logging to see the output and any potential errors
 logging.basicConfig(
@@ -15,25 +14,26 @@ logging.basicConfig(
 )
 
 
-def process_file(file_path: str, dest_dir: str) -> None:
+def process_file(file_path: Path, dest_dir: Path) -> None:
+    # noinspection PyBroadException
     try:
-        if file_path.endswith(PNG_FILE_EXT):
-            dest_file = os.path.join(dest_dir, Path(file_path).stem + JPG_FILE_EXT)
+        if file_path.suffix == PNG_FILE_EXT:
+            dest_file = dest_dir / (file_path.stem + JPG_FILE_EXT)
             logging.info(f'Converting png file "{file_path}" to "{dest_file}"...')
             copy_file_to_jpg(file_path, dest_file)
         else:
-            dest_file = os.path.join(dest_dir, os.path.basename(file_path))
+            dest_file = dest_dir / file_path.name
             logging.info(f'Copying file "{file_path}" to "{dest_file}"...')
             shutil.copy(file_path, dest_file)
 
     except FileNotFoundError:
-        logging.error(f'File not found during processing: "{file_path}".')
-    except Exception as e:
-        logging.error(f'An error occurred while processing "{file_path}": {e}')
+        logging.exception(f'File not found during processing: "{file_path}": ')
+    except Exception:
+        logging.exception(f'An error occurred while processing "{file_path}": ')
 
 
-def copy_file_to_jpg(srce_file: str, dest_file: str) -> None:
-    image = open_pil_image_for_reading(srce_file).convert("RGB")
+def copy_file_to_jpg(srce_file: Path, dest_file: Path) -> None:
+    image = open_pil_image_for_reading(str(srce_file)).convert("RGB")
 
     image.save(
         dest_file,
@@ -44,32 +44,32 @@ def copy_file_to_jpg(srce_file: str, dest_file: str) -> None:
 
 
 def traverse_and_process(
-    root_directory: str, dest_dir: str, file_processor_func: Callable[[str, str], None]
+    root_directory: Path, dest_dir: Path, file_processor_func: Callable[[Path, Path], None]
 ) -> None:
-    """
-    Traverses a directory tree and runs a processor function on each file.
+    """Traverses a directory tree and runs a processor function on each file.
 
     Args:
-        root_directory (str): The path to the top-level directory to start from.
-        dest_dir (str): The path to the destination directory.
+        root_directory (Path): The path to the top-level directory to start from.
+        dest_dir (Path): The path to the destination directory.
         file_processor_func (callable): The function to call for each file.
                                         It should accept one argument: the full file path.
+
     """
-    if not os.path.isdir(root_directory):
+    if not root_directory.is_dir():
         logging.error(f"The specified root directory does not exist: {root_directory}")
         return
 
     logging.info(f'Starting traversal of directory: "{root_directory}".')
     file_count = 0
-    for dirpath, _, filenames in os.walk(root_directory):
+    for dirpath, _, filenames in root_directory.walk():
         logging.info(f'Processing directory "{dirpath}"...')
-        dest_subdir = dirpath.replace(root_directory, dest_dir)
+        dest_subdir = Path(str(dirpath).replace(str(root_directory), str(dest_dir)))
         logging.info(f'Creating dest subdir "{dest_subdir}"...')
-        os.makedirs(dest_subdir, exist_ok=True)
+        dest_subdir.mkdir(parents=True, exist_ok=True)
 
         for filename in filenames:
             # Construct the full file path
-            full_path = os.path.join(dirpath, filename)
+            full_path = dirpath / filename
             # Run the provided function on the file
             file_processor_func(full_path, dest_subdir)
             file_count += 1
@@ -78,7 +78,7 @@ def traverse_and_process(
 
 
 if __name__ == "__main__":
-    png_dir = os.path.expanduser("~/Books/Carl Barks/Barks Panels Pngs")
-    jpg_dir = os.path.expanduser("~/Books/Carl Barks/Compleat Barks Disney Reader/Barks Panels")
+    png_dir = Path("~/Books/Carl Barks/Barks Panels Pngs").expanduser()
+    jpg_dir = Path("~/Books/Carl Barks/Compleat Barks Disney Reader/Barks Panels").expanduser()
 
     traverse_and_process(png_dir, jpg_dir, file_processor_func=process_file)
