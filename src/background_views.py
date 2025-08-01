d7bf5e30e8d55dcea0cae7b84a539b55826f6cdc
glk1001 <gregg.kay@gmail.com>
2025-08-01 15:57:24 +1000

    Trying out ruff on 'src/background_views.py'.

--- src/background_views.py
+++ src/background_views.py
@@ -1,35 +1,39 @@
+from __future__ import annotations
+
 import logging
 from enum import Enum, auto
-from typing import Dict, List, Union
-
-from kivy.clock import Clock
+from typing import TYPE_CHECKING
 
 from barks_fantagraphics.barks_tags import (
-    Tags,
-    TagGroups,
-    BARKS_TAGGED_TITLES,
     BARKS_TAG_GROUPS_TITLES,
+    BARKS_TAGGED_TITLES,
+    TagGroups,
+    Tags,
 )
-from barks_fantagraphics.barks_titles import Titles, BARKS_TITLES
+from barks_fantagraphics.barks_titles import BARKS_TITLES, Titles
 from barks_fantagraphics.comics_utils import get_abbrev_path
 from barks_fantagraphics.fanta_comics_info import (
-    FantaComicBookInfo,
-    FantaComicBookInfoDict,
     ALL_LISTS,
     SERIES_CS,
     SERIES_DDA,
-    SERIES_USA,
     SERIES_DDS,
-    SERIES_USS,
     SERIES_GG,
     SERIES_MISC,
+    SERIES_USA,
+    SERIES_USS,
+    FantaComicBookInfo,
+    FantaComicBookInfoDict,
 )
+from kivy.clock import Clock
+
 from filtered_title_lists import FilteredTitleLists
-from random_title_images import RandomTitleImages, ImageInfo, FileTypes, FIT_MODE_COVER
+from random_title_images import FIT_MODE_COVER, FileTypes, ImageInfo, RandomTitleImages
 from reader_colors import RandomColorTint
-from reader_consts_and_types import Color
-from reader_settings import ReaderSettings
 from reader_utils import get_formatted_color
+
+if TYPE_CHECKING:
+    from reader_consts_and_types import Color
+    from reader_settings import ReaderSettings
 
 TOP_VIEW_IMAGE_TYPES = {
     t for t in FileTypes if t not in [FileTypes.NONTITLE, FileTypes.ORIGINAL_ART]
@@ -83,9 +87,9 @@ class BackgroundViews:
         self,
         reader_settings: ReaderSettings,
         all_fanta_titles: FantaComicBookInfoDict,
-        title_lists: Dict[str, List[FantaComicBookInfo]],
+        title_lists: dict[str, list[FantaComicBookInfo]],
         random_title_images: RandomTitleImages,
-    ):
+    ) -> None:
         self._reader_settings = reader_settings
         self._all_fanta_titles = all_fanta_titles
         self._title_lists = title_lists
@@ -121,7 +125,7 @@ class BackgroundViews:
 
         self._view_state = ViewStates.PRE_INIT
 
-    def _get_fanta_info(self, title: Titles) -> Union[None, FantaComicBookInfo]:
+    def _get_fanta_info(self, title: Titles) -> None | FantaComicBookInfo:
         # TODO: Very roundabout way to get fanta info
         # TODO: And duplicated in main_screen
         title_str = BARKS_TITLES[title]
@@ -129,7 +133,7 @@ class BackgroundViews:
             return None
         return self._all_fanta_titles[title_str]
 
-    def _get_fanta_title_list(self, titles: List[Titles]) -> List[FantaComicBookInfo]:
+    def _get_fanta_title_list(self, titles: list[Titles]) -> list[FantaComicBookInfo]:
         fanta_title_list = [self._get_fanta_info(title) for title in titles]
         return [title for title in fanta_title_list if title]
 
@@ -169,16 +173,16 @@ class BackgroundViews:
     def set_current_category(self, cat: str) -> None:
         self._current_category = cat
 
-    def get_current_tag_group(self) -> Union[None, TagGroups]:
+    def get_current_tag_group(self) -> None | TagGroups:
         return self._current_tag_group
 
-    def set_current_tag_group(self, tag_group: Union[None, TagGroups]) -> None:
+    def set_current_tag_group(self, tag_group: None | TagGroups) -> None:
         self._current_tag_group = tag_group
 
-    def get_current_tag(self) -> Union[None, Tags]:
+    def get_current_tag(self) -> None | Tags:
         return self._current_tag
 
-    def set_current_tag(self, tag: Union[None, Tags]) -> None:
+    def set_current_tag(self, tag: None | Tags) -> None:
         self._current_tag = tag
 
     def get_current_year_range(self) -> str:
@@ -203,7 +207,7 @@ class BackgroundViews:
         self._view_state = view_state
         self._update_views()
 
-    def _update_views(self):
+    def _update_views(self) -> None:
         if self._view_state == ViewStates.PRE_INIT:
             self._top_view_image_opacity = 0.5
             self._set_top_view_image()
@@ -233,7 +237,7 @@ class BackgroundViews:
         self._set_bottom_view_fun_image()
         self._set_bottom_view_title_image_color()
 
-    def _set_top_view_image(self) -> None:
+    def _set_top_view_image(self) -> None:  # noqa: PLR0915
         # noinspection PyUnreachableCode
         match self._view_state:
             case ViewStates.PRE_INIT | ViewStates.INITIAL:
@@ -300,7 +304,7 @@ class BackgroundViews:
             case ViewStates.ON_INDEX_NODE:
                 self._set_top_view_image_for_index()
             case _:
-                assert False
+                raise AssertionError
 
         self._set_top_view_image_color()
         self._schedule_top_view_event()
@@ -314,43 +318,43 @@ class BackgroundViews:
             f" Opacity: {self._top_view_image_opacity}."
         )
 
-    def _set_initial_top_view_image(self):
+    def _set_initial_top_view_image(self) -> None:
         title = Titles.COLD_BARGAIN_A
         self._top_view_image_info = ImageInfo(
             self._reader_settings.file_paths.get_comic_inset_file(title), title, FIT_MODE_COVER
         )
 
-    def _set_top_view_image_for_intro(self):
+    def _set_top_view_image_for_intro(self) -> None:
         title = Titles.ADVENTURE_DOWN_UNDER
         self._top_view_image_info = ImageInfo(
             self._reader_settings.file_paths.get_comic_inset_file(title), title, FIT_MODE_COVER
         )
 
-    def _set_top_view_image_for_stories(self):
+    def _set_top_view_image_for_stories(self) -> None:
         self._top_view_image_info = self._get_top_view_random_image(self._title_lists[ALL_LISTS])
 
-    def _set_top_view_image_for_cs(self):
+    def _set_top_view_image_for_cs(self) -> None:
         self._top_view_image_info = self._get_top_view_random_image(self._title_lists[SERIES_CS])
 
-    def _set_top_view_image_for_dd(self):
+    def _set_top_view_image_for_dd(self) -> None:
         self._top_view_image_info = self._get_top_view_random_image(self._title_lists[SERIES_DDA])
 
-    def _set_top_view_image_for_us(self):
+    def _set_top_view_image_for_us(self) -> None:
         self._top_view_image_info = self._get_top_view_random_image(self._title_lists[SERIES_USA])
 
-    def _set_top_view_image_for_dds(self):
+    def _set_top_view_image_for_dds(self) -> None:
         self._top_view_image_info = self._get_top_view_random_image(self._title_lists[SERIES_DDS])
 
-    def _set_top_view_image_for_uss(self):
+    def _set_top_view_image_for_uss(self) -> None:
         self._top_view_image_info = self._get_top_view_random_image(self._title_lists[SERIES_USS])
 
-    def _set_top_view_image_for_gg(self):
+    def _set_top_view_image_for_gg(self) -> None:
         self._top_view_image_info = self._get_top_view_random_image(self._title_lists[SERIES_GG])
 
-    def _set_top_view_image_for_misc(self):
+    def _set_top_view_image_for_misc(self) -> None:
         self._top_view_image_info = self._get_top_view_random_image(self._title_lists[SERIES_MISC])
 
-    def _set_top_view_image_for_category(self):
+    def _set_top_view_image_for_category(self) -> None:
         logging.debug(f"Current category: '{self._current_category}'.")
         if not self._current_category:
             title = Titles.GOOD_NEIGHBORS
@@ -362,7 +366,7 @@ class BackgroundViews:
                 self._title_lists[self._current_category]
             )
 
-    def _set_top_view_image_for_tag_group(self):
+    def _set_top_view_image_for_tag_group(self) -> None:
         logging.debug(f"Current tag_group: '{self._current_tag_group}'.")
         if not self._current_tag_group:
             title = Titles.GOOD_NEIGHBORS
@@ -375,7 +379,7 @@ class BackgroundViews:
             )
             self._top_view_image_info = self._get_top_view_random_image(fanta_title_list)
 
-    def _set_top_view_image_for_tag(self):
+    def _set_top_view_image_for_tag(self) -> None:
         logging.debug(f"Current tag: '{self._current_tag}'.")
         if not self._current_tag:
             title = Titles.GOOD_NEIGHBORS
@@ -386,7 +390,7 @@ class BackgroundViews:
             fanta_title_list = self._get_fanta_title_list(BARKS_TAGGED_TITLES[self._current_tag])
             self._top_view_image_info = self._get_top_view_random_image(fanta_title_list)
 
-    def _set_top_view_image_for_year_range(self):
+    def _set_top_view_image_for_year_range(self) -> None:
         logging.debug(f"Year range: '{self._current_year_range}'.")
         if not self._current_year_range:
             title = Titles.GOOD_NEIGHBORS
@@ -398,7 +402,7 @@ class BackgroundViews:
                 self._title_lists[self._current_year_range]
             )
 
-    def _set_top_view_image_for_cs_year_range(self):
+    def _set_top_view_image_for_cs_year_range(self) -> None:
         logging.debug(f"CS Year range: '{self._current_cs_year_range}'.")
         if not self._current_cs_year_range:
             title = Titles.GOOD_NEIGHBORS
@@ -410,7 +414,7 @@ class BackgroundViews:
             logging.debug(f"CS Year range key: '{cs_range}'.")
             self._top_view_image_info = self._get_top_view_random_image(self._title_lists[cs_range])
 
-    def _set_top_view_image_for_us_year_range(self):
+    def _set_top_view_image_for_us_year_range(self) -> None:
         logging.debug(f"US Year range: '{self._current_us_year_range}'.")
         if not self._current_us_year_range:
             title = Titles.BACK_TO_THE_KLONDIKE
@@ -422,27 +426,27 @@ class BackgroundViews:
             logging.debug(f"US Year range key: '{us_range}'.")
             self._top_view_image_info = self._get_top_view_random_image(self._title_lists[us_range])
 
-    def _get_top_view_random_image(self, title_list: List[FantaComicBookInfo]):
+    def _get_top_view_random_image(self, title_list: list[FantaComicBookInfo]) -> ImageInfo:
         return self._random_title_images.get_random_image(
             title_list, file_types=TOP_VIEW_IMAGE_TYPES, use_edited_only=True
         )
 
-    def _set_top_view_image_for_search(self):
+    def _set_top_view_image_for_search(self) -> None:
         self._top_view_image_info = self._random_title_images.get_random_search_image()
 
-    def _set_top_view_image_for_appendix(self):
+    def _set_top_view_image_for_appendix(self) -> None:
         title = Titles.FABULOUS_PHILOSOPHERS_STONE_THE
         self._top_view_image_info = ImageInfo(
             self._reader_settings.file_paths.get_comic_inset_file(title), title, FIT_MODE_COVER
         )
 
-    def _set_top_view_image_for_index(self):
+    def _set_top_view_image_for_index(self) -> None:
         title = Titles.TRUANT_OFFICER_DONALD
         self._top_view_image_info = ImageInfo(
             self._reader_settings.file_paths.get_comic_inset_file(title), title, FIT_MODE_COVER
         )
 
-    def _set_top_view_image_color(self):
+    def _set_top_view_image_color(self) -> None:
         self._top_view_image_color = self._top_view_image_random_color_tint.get_random_color()
 
     def _set_bottom_view_fun_image(self) -> None:
@@ -470,7 +474,7 @@ class BackgroundViews:
             f" Opacity: {self._bottom_view_fun_image_opacity}."
         )
 
-    def _get_fun_image_titles(self) -> List[FantaComicBookInfo]:
+    def _get_fun_image_titles(self) -> list[FantaComicBookInfo]:
         if not DEBUG_FUN_IMAGE_TITLES:
             return self._title_lists[ALL_LISTS]
 
@@ -482,7 +486,7 @@ class BackgroundViews:
 
     # TODO: Rationalize image color setters - make more responsive to individual images
     #       have fun images weighted to larger opacity and full color
-    def _set_bottom_view_fun_image_color(self):
+    def _set_bottom_view_fun_image_color(self) -> None:
         self._bottom_view_fun_image_color = (
             self._bottom_view_fun_image_random_color_tint.get_random_color()
         )
@@ -491,14 +495,14 @@ class BackgroundViews:
         self._bottom_view_title_image_info.filename = image_file
         self._log_bottom_view_title_state()
 
-    def _set_bottom_view_title_image_color(self):
+    def _set_bottom_view_title_image_color(self) -> None:
         self._bottom_view_title_image_color = (
             self._bottom_view_title_image_random_color_tint.get_random_color()
         )
 
         self._log_bottom_view_title_state()
 
-    def _log_bottom_view_title_state(self):
+    def _log_bottom_view_title_state(self) -> None:
         logging.debug(
             f"Bottom view title image:"
             f" State: {self._view_state},"
@@ -508,18 +512,18 @@ class BackgroundViews:
             f" Opacity: {self._bottom_view_title_opacity}."
         )
 
-    def _schedule_top_view_event(self):
+    def _schedule_top_view_event(self) -> None:
         if self._top_view_change_event:
             self._top_view_change_event.cancel()
 
         self._top_view_change_event = Clock.schedule_interval(
-            lambda dt: self._set_top_view_image(), self.TOP_VIEW_EVENT_TIMEOUT_SECS
+            lambda _dt: self._set_top_view_image(), self.TOP_VIEW_EVENT_TIMEOUT_SECS
         )
 
-    def _schedule_bottom_view_fun_image_event(self):
+    def _schedule_bottom_view_fun_image_event(self) -> None:
         if self._bottom_view_change_fun_image_event:
             self._bottom_view_change_fun_image_event.cancel()
 
         self._bottom_view_change_fun_image_event = Clock.schedule_interval(
-            lambda dt: self._set_bottom_view_fun_image(), self.BOTTOM_VIEW_EVENT_TIMEOUT_SECS
+            lambda _dt: self._set_bottom_view_fun_image(), self.BOTTOM_VIEW_EVENT_TIMEOUT_SECS
         )
