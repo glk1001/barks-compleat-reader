6d879214e68abef12858a4ebfe4cbb081461243e
glk1001 <gregg.kay@gmail.com>
2025-08-02 20:02:10 +1000

    Trying out ruff on 'src/test-logging.py'.

--- src/test-logging.py
+++ src/test-logging.py
@@ -1,11 +1,9 @@
 import logging
 import os
 
-# os.environ["KIVY_LOG_MODE"] = "PYTHON"
 os.environ["KIVY_LOG_MODE"] = "MIXED"
 
 from barks_fantagraphics.comics_logging import setup_logging
-
 
 setup_logging(log_level=logging.DEBUG)
 
