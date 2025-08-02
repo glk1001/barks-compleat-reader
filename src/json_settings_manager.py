afcbfadfc6cf0bf1e9d6dbb99fb703f1c1712257
glk1001 <gregg.kay@gmail.com>
2025-08-02 10:55:17 +1000

    Trying out ruff on 'src/json_settings_manager.py'.

--- src/json_settings_manager.py
+++ src/json_settings_manager.py
@@ -1,17 +1,18 @@
+from __future__ import annotations
+
 import json
 from dataclasses import dataclass
-from typing import Union, List, Dict, Any, Self
-
-from kivy.storage.jsonstore import JsonStore
+from typing import Any, Self
 
 from barks_fantagraphics.comics_consts import PageType
+from kivy.storage.jsonstore import JsonStore
 
 _READER_SETTINGS = "AAA_Settings"
 _READER_SETTING_LAST_SELECTED_NODE = "last_selected_node"
 _TITLE_SETTING_LAST_READ_PAGE = "last_read_page"
 
 
-JsonSavedPageInfo = Dict[str, Any]
+JsonSavedPageInfo = dict[str, Any]
 
 
 @dataclass
@@ -42,27 +43,27 @@ class SavedPageInfo:
 class SettingsManager:
     """Handles saving and loading of user settings and progress to a JSON store."""
 
-    def __init__(self, store_path: str):
+    def __init__(self, store_path: str) -> None:
         self._store = JsonStore(store_path)
 
-    def get_last_selected_node_path(self) -> Union[List[str], None]:
-        """Retrieves the path of the last selected node."""
+    def get_last_selected_node_path(self) -> list[str] | None:
+        """Retrieve the path of the last selected node."""
         if not self._store.exists(_READER_SETTINGS):
             return None
         raw_path = self._store.get(_READER_SETTINGS).get(_READER_SETTING_LAST_SELECTED_NODE)
         return json.loads(raw_path) if raw_path else None
 
-    def save_last_selected_node_path(self, path: List[str]) -> None:
-        """Saves the path of the last selected node."""
+    def save_last_selected_node_path(self, path: list[str]) -> None:
+        """Save the path of the last selected node."""
         self._store.put(_READER_SETTINGS, **{_READER_SETTING_LAST_SELECTED_NODE: json.dumps(path)})
 
-    def get_last_read_page(self, title_str: str) -> Union[SavedPageInfo, None]:
-        """Retrieves the last read page information for a specific title."""
+    def get_last_read_page(self, title_str: str) -> SavedPageInfo | None:
+        """Retrieve the last read page information for a specific title."""
         if not self._store.exists(title_str):
             return None
         json_info = self._store.get(title_str).get(_TITLE_SETTING_LAST_READ_PAGE)
         return SavedPageInfo.from_json(json_info) if json_info else None
 
     def save_last_read_page(self, title_str: str, page_info: SavedPageInfo) -> None:
-        """Saves the last read page information for a specific title."""
+        """Save the last read page information for a specific title."""
         self._store.put(title_str, **{_TITLE_SETTING_LAST_READ_PAGE: page_info.to_json()})
