19eee5cf36a9760ae8d376639bf1791c50382a6f
glk1001 <gregg.kay@gmail.com>
2025-08-03 10:17:38 +1000

    Trying out ruff on 'src/export-reader-files.py'.

--- src/export-reader-files.py
+++ src/export-reader-files.py
@@ -1,19 +1,18 @@
 import logging
-import os.path
 import sys
 from pathlib import Path
-from typing import List
 
 from barks_fantagraphics.comic_book import ComicBook
-from barks_fantagraphics.comics_cmd_args import CmdArgs, CmdArgNames
-from barks_fantagraphics.comics_consts import PageType, JPG_FILE_EXT
+from barks_fantagraphics.comics_cmd_args import CmdArgNames, CmdArgs
+from barks_fantagraphics.comics_consts import JPG_FILE_EXT, PageType
 from barks_fantagraphics.comics_logging import setup_logging
 from barks_fantagraphics.comics_utils import get_abbrev_path
-from barks_fantagraphics.pil_image_utils import open_pil_image_for_reading, SAVE_JPG_COMPRESS_LEVEL
+from barks_fantagraphics.pil_image_utils import SAVE_JPG_COMPRESS_LEVEL, open_pil_image_for_reading
+
 from reader_file_paths import _DEFAULT_PNG_BARKS_PANELS_DIR
 
-inset_files_dir = os.path.join(_DEFAULT_PNG_BARKS_PANELS_DIR, "Insets")
-comic_cover_files_dir = os.path.join(_DEFAULT_PNG_BARKS_PANELS_DIR, "Covers")
+inset_files_dir = _DEFAULT_PNG_BARKS_PANELS_DIR / "Insets"
+comic_cover_files_dir = _DEFAULT_PNG_BARKS_PANELS_DIR / "Covers"
 
 
 def export_reader_files_for_title(ttl: str) -> None:
@@ -26,8 +25,8 @@ def export_reader_files_for_title(ttl: str) -> None:
     export_page_files_as_title_files(ttl, comic, comic_cover_files_dir, [PageType.COVER])
 
 
-def export_inset_file(ttl: str, inset_file: str) -> None:
-    dest_file = os.path.join(inset_files_dir, ttl + JPG_FILE_EXT)
+def export_inset_file(ttl: str, inset_file: Path) -> None:
+    dest_file = inset_files_dir / (ttl + JPG_FILE_EXT)
 
     logging.info(f'Exporting "{get_abbrev_path(inset_file)}" to "{get_abbrev_path(dest_file)}".')
 
@@ -35,7 +34,7 @@ def export_inset_file(ttl: str, inset_file: str) -> None:
 
 
 def export_page_files_as_title_files(
-    ttl: str, comic: ComicBook, dest_dir: str, page_types: List[PageType]
+    ttl: str, comic: ComicBook, dest_dir: Path, page_types: list[PageType]
 ) -> None:
     pages = comic.get_final_srce_story_files(page_types)
     if len(pages) == 0:
@@ -43,9 +42,10 @@ def export_page_files_as_title_files(
 
     assert len(pages) == 1
 
-    os.makedirs(dest_dir, exist_ok=True)
+    dest_dir.mkdir(parents=True, exist_ok=True)
     srce_file, is_modified = pages[0]
-    dest_file = os.path.join(dest_dir, ttl + JPG_FILE_EXT)
+    srce_file = Path(srce_file)
+    dest_file = dest_dir / (ttl + JPG_FILE_EXT)
 
     logging.info(
         f'Exporting {page_types} file "{get_abbrev_path(srce_file)}"'
@@ -56,17 +56,18 @@ def export_page_files_as_title_files(
 
 
 def export_page_files_to_title_dirs(
-    ttl: str, comic: ComicBook, dest_dir: str, page_types: List[PageType]
+    ttl: str, comic: ComicBook, dest_dir: Path, page_types: list[PageType]
 ) -> None:
     pages = comic.get_final_srce_story_files(page_types)
     if len(pages) == 0:
         return
 
-    ttl_dir = os.path.join(dest_dir, ttl)
-    os.makedirs(ttl_dir, exist_ok=True)
+    ttl_dir = dest_dir / ttl
+    ttl_dir.mkdir(parents=True, exist_ok=True)
 
-    for srce_file, is_modified in pages:
-        dest_file = os.path.join(ttl_dir, Path(srce_file).stem + JPG_FILE_EXT)
+    for file, _is_modified in pages:
+        srce_file = Path(file)
+        dest_file = ttl_dir / (srce_file.stem + JPG_FILE_EXT)
 
         logging.info(
             f'Exporting {page_types} file "{get_abbrev_path(srce_file)}"'
@@ -76,8 +77,8 @@ def export_page_files_to_title_dirs(
         copy_image_file(srce_file, dest_file)
 
 
-def copy_image_file(srce: str, dest: str) -> None:
-    image = open_pil_image_for_reading(srce).convert("RGB")
+def copy_image_file(srce: Path, dest: Path) -> None:
+    image = open_pil_image_for_reading(str(srce)).convert("RGB")
 
     image.save(
         dest,
