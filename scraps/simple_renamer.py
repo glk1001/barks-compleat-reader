87b629b17ad05cfdf64888d205fdbfeb068d83a8
glk1001 <gregg.kay@gmail.com>
2025-11-30 14:39:58 +1100

    Used simple_renamer.

--- scraps/simple_renamer.py
+++ scraps/simple_renamer.py
@@ -1,25 +1,23 @@
 from pathlib import Path
 
 # ext = ".jpg"
-#ext = ".png"
-ext = ".json"
+ext = ".png"
+# ext = ".json"
 # ext= ".svg"
 # ext= ".svg.png"
-for srce_file in Path.cwd().iterdir():
-    file_str = srce_file.stem
-    if file_str.startswith("batch-job-details-"):
-        dest_file = Path(file_str[len("batch-job-details-"):] + "-batch-job-details.json")
-    elif file_str.startswith("batch-requests-with-image-"):
-        dest_file = Path(file_str[len("batch-requests-with-image-"):] + "-batch-requests-with-image.json")
-    else:
-        print(f"Skipping: {file_str}")
+for srce_dir in Path.cwd().iterdir():
+    if not srce_dir.is_dir():
         continue
+    for srce_file in srce_dir.iterdir():
+        if "final" not in srce_file.name:
+            continue
+        file_str = str(srce_file).replace("final-text", "prelim")
+        dest_file = Path(file_str)
+    #for i in range(7,31):
+        # srce_file = Path(f"{i:03d}-gemini-final-groups-paddleocr{ext}")
+        # if not srce_file.is_file():
+        #     raise FileNotFoundError(srce_file)
+        # dest_file = Path(f"{i:03d}-paddleocr-gemini-final-groups{ext}")
 
-#for i in range(7,31):
-    # srce_file = Path(f"{i:03d}-gemini-final-groups-paddleocr{ext}")
-    # if not srce_file.is_file():
-    #     raise FileNotFoundError(srce_file)
-    # dest_file = Path(f"{i:03d}-paddleocr-gemini-final-groups{ext}")
-
-    srce_file.rename(dest_file)
-    print(f'Rename: "{srce_file}" -> "{dest_file}"')
+        srce_file.rename(dest_file)
+        print(f'Rename: "{srce_file}" -> "{dest_file}"')
