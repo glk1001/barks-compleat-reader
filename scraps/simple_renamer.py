384c3163f5d9d6850b00eebd269524631a721140
glk1001 <gregg.kay@gmail.com>
2025-12-09 14:00:38 +1100

    Another use of simple renamer.

--- scraps/simple_renamer.py
+++ scraps/simple_renamer.py
@@ -5,19 +5,21 @@ ext = ".png"
 # ext = ".json"
 # ext= ".svg"
 # ext= ".svg.png"
-for srce_dir in Path.cwd().iterdir():
-    if not srce_dir.is_dir():
+# for srce_dir in Path.cwd().iterdir():
+#     if not srce_dir.is_dir():
+#         continue
+#    for srce_file in srce_dir.iterdir():
+for srce_file in Path.cwd().iterdir():
+    if "final" not in srce_file.name:
         continue
-    for srce_file in srce_dir.iterdir():
-        if "final" not in srce_file.name:
-            continue
-        file_str = str(srce_file).replace("final-text", "prelim")
-        dest_file = Path(file_str)
-    #for i in range(7,31):
-        # srce_file = Path(f"{i:03d}-gemini-final-groups-paddleocr{ext}")
-        # if not srce_file.is_file():
-        #     raise FileNotFoundError(srce_file)
-        # dest_file = Path(f"{i:03d}-paddleocr-gemini-final-groups{ext}")
+    file_str = str(srce_file).replace("final", "prelim")
+    dest_file = Path(file_str)
 
-        srce_file.rename(dest_file)
-        print(f'Rename: "{srce_file}" -> "{dest_file}"')
+#for i in range(7,31):
+    # srce_file = Path(f"{i:03d}-gemini-final-groups-paddleocr{ext}")
+    # if not srce_file.is_file():
+    #     raise FileNotFoundError(srce_file)
+    # dest_file = Path(f"{i:03d}-paddleocr-gemini-final-groups{ext}")
+
+    srce_file.rename(dest_file)
+    print(f'Rename: "{srce_file}" -> "{dest_file}"')
